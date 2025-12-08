a392591e0e5c135f7721399553e8d751c1df7c39
uthfol-sust <148445708+uthfol-sust@users.noreply.github.com>
2025-12-08 11:57:54 +0600

    write docker file

--- IntroDockerfile/main.go
+++ IntroDockerfile/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-
 	fmt.Println("Intro to docker file")
 
-    
+    mux := http.NewServeMux()
+
+   
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, Docker!"))
+	})
+
+    fmt.Println("Server Running on Port : 8080..")
+	http.ListenAndServe(":8080",mux)
 }
